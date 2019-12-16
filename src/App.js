d013bfbd3668ab22603a02ff68a182839f6663a7
Naveenraj <naveenraj436@gmail.com>
2019-12-16 16:33:04 +0530

    okrjoy styles added and code improved

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
 import React from "react";
 import Notifications from "cb-react-notifications";
-import "./App.css";
-import "./styles.css";
+import "./App.scss";
+// import "./styles.css";
+import "./okrjoy-styles.scss";
+import CustomComponent from "./CustomComponent/CustomComponent";
 function App() {
   const markAllAsRead = () => {
     console.log("markAllAsRead");
   };
   const dataLink =
     "https://raw.githubusercontent.com/NaveenrajV/cb-notifications/master/Data.json";
+
+  const data = {
+    "1": {
+      image:
+        "https://synergi-dev.s3.ap-southeast-1.amazonaws.com/profile-pictures/6b924382abac8a886ddf18d1b54b4aad-7eb8ac92ca10898bd6cc31ca6e356c45.png",
+      message: "Kameshwaran S had shared a feedback with you.",
+      detailPage: "/"
+    },
+    "2": {
+      image:
+        "https://synergi-dev.s3.ap-southeast-1.amazonaws.com/profile-pictures/6b924382abac8a886ddf18d1b54b4aad-7eb8ac92ca10898bd6cc31ca6e356c45.png",
+      message: "Kameshwaran S had shared a feedback with you.",
+      detailPage: "/"
+    }
+    // "3": {
+    //   image:
+    //     "https://synergi-dev.s3.ap-southeast-1.amazonaws.com/profile-pictures/6b924382abac8a886ddf18d1b54b4aad-7eb8ac92ca10898bd6cc31ca6e356c45.png",
+    //   message: "Kameshwaran S had shared a feedback with you.",
+    //   detailPage: "/"
+    // },
+    // "4": {
+    //   image:
+    //     "https://synergi-dev.s3.ap-southeast-1.amazonaws.com/profile-pictures/6b924382abac8a886ddf18d1b54b4aad-7eb8ac92ca10898bd6cc31ca6e356c45.png",
+    //   message: "Kameshwaran S had shared a feedback with you.",
+    //   detailPage: "/"
+    // },
+    // "5": {
+    //   image:
+    //     "https://synergi-dev.s3.ap-southeast-1.amazonaws.com/profile-pictures/6b924382abac8a886ddf18d1b54b4aad-7eb8ac92ca10898bd6cc31ca6e356c45.png",
+    //   message: "Kameshwaran S had shared a feedback with you.",
+    //   detailPage: "/"
+    // },
+    // "6": {
+    //   image:
+    //     "https://synergi-dev.s3.ap-southeast-1.amazonaws.com/profile-pictures/6b924382abac8a886ddf18d1b54b4aad-7eb8ac92ca10898bd6cc31ca6e356c45.png",
+    //   message: "Kameshwaran S had shared a feedback with you.",
+    //   detailPage: "/"
+    // }
+  };
+
   return (
     <div className="App">
       <div>Item 1</div>
       <div>Item 2</div>
       <div>
         <Notifications
-          data={dataLink}
+          // renderItem={CustomComponent}
+          data={data}
+          header={{ title: "Notifications", option: "View All" }}
+          displaySeeAll={false}
           markAllAsRead={markAllAsRead}
           links={{ seeAll: "/seeAll" }}
-          // classNamePrefix="notify-"
+          classNamePrefix="okrjoy-"
+          cardOptions={false}
         />
       </div>
       <div>Item 4</div>
